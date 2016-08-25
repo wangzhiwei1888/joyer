a04ed8abade3f622dcf33aea75f68b6189e7ad41
majun1@jd.com <majun1@jd.com>
2016-08-25 20:21:39 +0800

    fix  unzip no permissions

--- gulpfile.js/tasks/zip.js
+++ gulpfile.js/tasks/zip.js
@@ -3,6 +3,7 @@ var config = require(program.config || require('./../joyer.config')());
 
 var gulp = require('gulp');
 var zip = require('gulp-zip');
+var tap = require('gulp-tap');
 var path = require('path');
 
 var dirname = path.dirname(config.root.dest);
@@ -12,6 +13,11 @@ var name = config.root.dest.replace(dirname, '').replace('/', '') + '.zip';
 // console.log(name);
 gulp.task('zip', function() {
   return gulp.src(path.join(process.env.CWD_PATH, config.root.dest, '/**/*'))
+    .pipe(tap(function(file) {
+      if (file.isDirectory()) {
+        file.stat.mode = parseInt('40777', 8);
+      }
+    }))
     .pipe(zip(name))
     .pipe(gulp.dest(path.join(process.env.CWD_PATH, config.root.dest)));
 });
