229a725ce7afcb4063130f60ca8d4f7f25dbf394
majun1@jd.com <majun1@jd.com>
2016-12-14 20:38:10 +0800

    update

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -5,11 +5,15 @@ if (!config.tasks.css) return;
 
 var process = require('process');
 var gulp = require('gulp');
+var gulpif = require('gulp-if');
 var browserSync = require('browser-sync');
 var compass = require('gulp-compass');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 var handleErrors = require('../lib/handleErrors');
 var path = require('path');
+var cssnano = require('gulp-cssnano');
 
 var paths = {
   src: path.join(process.env.CWD_PATH, config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
@@ -17,33 +21,50 @@ var paths = {
 };
 
 // console.log('sass', paths.src)
+if (config.compass !== false) {
+  var compassConfig = {
+    // config_file: './config.rb',
+    // environment: 'development'
+    // sourcemap: true
+    sass: path.join(process.env.CWD_PATH, config.root.src, config.tasks.css.src),
+    image: path.join(process.env.CWD_PATH, config.root.src, 'images'),
+    css: path.join(process.env.CWD_PATH, config.root.src, '.styles'),
+    require: ['compass/import-once/activate'],
+  };
 
-var compassConfig = {
-  // config_file: './config.rb',
-  // environment: 'development'
-  // sourcemap: true
-  sass: path.join(process.env.CWD_PATH, config.root.src, config.tasks.css.src),
-  image: path.join(process.env.CWD_PATH, config.root.src, 'images'),
-  css: path.join(process.env.CWD_PATH, config.root.src, '.styles'),
-  require: ['compass/import-once/activate'],
-};
 
+  if (process.env.NODE_ENV === 'production') {
+    compassConfig.environment = 'production';
+  } else {
+    compassConfig.comments = true;
+  }
 
-if (process.env.NODE_ENV === 'production') {
-  compassConfig.environment = 'production';
+  gulp.task('css', function() {
+    return gulp.src(paths.src)
+      .pipe(sourcemaps.init())
+      .pipe(compass(compassConfig))
+      .on('error', handleErrors)
+      // .pipe(autoprefixer(config.tasks.css.autoprefixer))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(paths.dest))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+  });
 } else {
-  compassConfig.comments = true;
+  gulp.task('css', function() {
+    return gulp.src(paths.src)
+      .pipe(gulpif(!global.production, sourcemaps.init()))
+      .pipe(sass(config.tasks.css.sass))
+      .on('error', handleErrors)
+      .pipe(autoprefixer(config.tasks.css.autoprefixer))
+      .pipe(gulpif(global.production, cssnano({
+        autoprefixer: false
+      })))
+      .pipe(gulpif(!global.production, sourcemaps.write()))
+      .pipe(gulp.dest(paths.dest))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+  })
 }
-
-gulp.task('css', function() {
-  return gulp.src(paths.src)
-    .pipe(sourcemaps.init())
-    .pipe(compass(compassConfig))
-    .on('error', handleErrors)
-    // .pipe(autoprefixer(config.tasks.css.autoprefixer))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.dest))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
