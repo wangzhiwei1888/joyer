a270eb44aaafa12740499f5743dc1ffe86071730
majun1@jd.com <majun1@jd.com>
2017-01-23 11:54:25 +0800

    update

--- gulpfile.js/lib/getEnabledTasks.js
+++ gulpfile.js/lib/getEnabledTasks.js
@@ -1,18 +1,19 @@
 var compact = require('lodash/array/compact');
 var program = require('commander');
 var config = require(program.config || require('./../joyer.config')());
-  // Grouped by what can run in parallel
-var assetTasks = ['fonts', 'iconFont', 'images', 'svgSprite'];
+// Grouped by what can run in parallel
+// var assetTasks = ['fonts', 'iconFont', 'images', 'svgSprite'];
+var assetTasks = ['images', 'svgSprite'];
 var codeTasks = ['html', 'css', 'js'];
 
-module.exports = function(env) {
+module.exports = function (env) {
   var jsTasks = {
     watch: 'webpack:watch',
     development: 'webpack:development',
     production: 'webpack:production'
   };
 
-  var matchFilter = function(task) {
+  var matchFilter = function (task) {
     if (config.tasks[task]) {
       if (task === 'js') {
         task = jsTasks[env] || jsTask.watch;
